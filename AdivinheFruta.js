7e64541233ffba5ed1e9430c0528de19697ae1c9
Suyanne <maria.suiane934@gmail.com>
2023-05-25 23:17:50 -0300

    Projeto de Estudos

---
+++ AdivinheFruta.js
@@ -0,0 +1,32 @@
+const readline = require('prompt-sync')({sigint:true}); //serve para otimizar o codigo na hora de escrever
+const palavras = ['Abacate','Abacaxi','Açaí','Acerola','Ameixa','Amora','Ananás','Banana','Cacau','Caqui','Carambola','Cereja','Coco','Cranberry','Damasco','Figo','Framboesa','Goiaba','Groselha','Jabuticaba','Jaca','Jambo','Kiwi','Laranja','Limão','Maçã','Mamão','Manga','Maracujá','Melancia','Melão','Mirtilo','Morango','Pera','Pêssego','Pitanga','Romã','Tangerina','Uva','Atemoia','Bergamota','Cajá','Caju','Cambuci','Camu-camu','Caramboleira','Chirimoya','Dovyalis','Eritrina','Fedegoso','Feijoa','Graviola','Guabiju','Guabiroba','Guaraná','Iba','Ingá','Ipê-roxo','Jabuticabeira','Jamelão','Jatobá','Juá','Lichia','Mabolo','Marmelo','Nêspera','Noni','Olho-de-cabra','Pata-de-vaca','Pequi','Pindaíba','Piquiá','Quixaba','Rambutan','Rosal','Sapoti','Siriguela','Tamarindo','Uvaia','Veludo','Xixá','Zimbro','Zimbro-da-praia','Zimbro-de-jardim','Zimbro-dos-montes','Ziziphus','Zumbi'];
+const palavraSecreta = palavras[Math.floor(Math.random() * palavras.length)]; //math.radom ele gera um numero aleatorio entre 0 e 1, math.floar serve para arredondar para baixo e obter um indice calido dentro do array
+console.log('Bem-vindo ao jogo de adivinhação de palavras!');
+console.log('Adivinhe a fruta secreta.');
+var tentativas = 4; // correspondente ao numero de tenativas restantes que o user tera 
+var acertou = false;
+
+while(tentativas > 0){
+    var palpite = readline('Digite seu Palpite:  ');
+    var userAcertou = verificarPalpite(palpite.toLocaleLowerCase());
+    console.log(userAcertou);
+    if (userAcertou === 'Parabéns você acertou a fruta premiada'){
+        acertou = true; 
+        break;
+    } // se o usuario acertar a fruta que foi escolhida pela maquina o codigo ira parar aqui ISSO AQUI E O BREAK NAO A DEFINICAO QUE O USUARIO GANHOU
+    tentativas--;
+ } //Nesse contexto específico, a variável tentativas é utilizada para controlar o número de tentativas restantes para adivinhar a palavra secreta. A cada iteração do loop, essa instrução é executada, diminuindo o valor de tentativas em 1. Isso significa que, a cada tentativa do usuário, o número de chances restantes é reduzido.
+    if (acertou){
+        console.log('Parabéns! Você acertou a palavra secreta.');
+    }
+    else {
+        console.log('Suas tentativas acabaram. A palavra secreta era: ' + palavraSecreta);
+    }
+function verificarPalpite(palpite){
+    if (palpite === palavraSecreta){
+        return 'Parabéns você acertou a fruta premiada'
+    }
+    else {
+        return 'Você errou! Você possui apenas ' + tentativas + ' chances'
+    }
+}
---
+++ estudos.js
@@ -0,0 +1,57 @@
+// var a = 20
+// var b = a * 2;
+// var c = a / 2;
+// var d = a - 2;
+// var e = a + 2;
+// var f = Number(a)
+// console.log ("resultado " + f );
+
+// var fruta = "Batata";
+// var preco = "3,50";
+// var levar = true;
+// var novoValor; 
+// console.log(fruta + " " + preco + " " + levar + " " + novoValor);
+
+//antecessor e posterior
+// const readline = require ('prompt-sync')({sigint:true});
+// // Solicita ao usuário para digitar um número
+// var num = parseFloat(readline('Digite um número: ')); //indica que um numero vai ser passado
+// // Calcula o antecessor e o sucessor
+// var antecessor = num - 1;
+// var sucessor = num + 1;
+// // Exibe os resultados
+// console.log('O número anterior a esse e: ' + antecessor);
+// console.log('O numero posterior a esse e: ' + sucessor);
+
+//restaurante
+// const readline = require('prompt-sync')({sigint:true});
+// var total = parseFloat(readline('Digite o total da conta: '))
+// var numeroPessoas = parseFloat(readline('Essa conta será dividida em quantas pessoas? '));
+// var valorCliente = total/numeroPessoas
+// console.log('Valor da conta: ' + total)
+// console.log('Número de cliente: ' + numeroPessoas)
+// console.log('Valor por cliente: ' + valorCliente)
+
+//loja
+// const readline = require('prompt-sync')({sigint:true});
+// var precoProduto = parseFloat(readline('Quanto o produto custa? '));
+// var valorDesconto = 0.10;
+// var desconto = precoProduto * valorDesconto; //foi criada esta variavel para servir de apoio para fazer o calculo do desconto
+// var precoVista = precoProduto - desconto;
+// var precoParcelado = precoProduto / 3;
+// console.log('Preço R$: ' + precoProduto);
+// console.log('À Vista R$: ' + precoVista);
+// console.log('À Prazo em 3x R$: ' + precoParcelado);
+
+
+//media
+// const readline  = require('prompt-sync')({sigint:true});
+// var nota1 = parseFloat(readline('Qual sua nota em Matématica? '));
+// var nota2 = parseFloat(readline('Qual sua nota em Química? '));
+// var total = nota1 + nota2;
+// var media = total / 2;
+// console.log('1° Nota: ' + nota1)
+// console.log('2° Nota: ' + nota2)
+// console.log('Média: ' + media)
+
+
